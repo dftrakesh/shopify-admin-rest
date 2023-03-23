3901bd0790fe441c87f3c4a77bf0754abc70a9eb
milan.lathiya <milan.lathiya@dakshyafreondtech.com>
2023-03-23 10:02:37 +0530

    MAN-2 Created InventoryLevelsAPI.

---
+++ src/main/java/com/dft/api/shopify/model/ShopifyInventoryLevelsRoot.java
@@ -0,0 +1,16 @@
+package com.dft.api.shopify.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ShopifyInventoryLevelsRoot {
+
+	private List<ShopifyInventoryLevel> inventoryLevels;
+}
