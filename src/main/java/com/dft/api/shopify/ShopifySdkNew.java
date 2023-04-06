717013908a3c799880fc1786bad4cee4a93cb2a3
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2023-04-06 15:37:02 +0530

    TB-31 Shopify new Sdk implement,get token and get shop method in shopify sdk

---
+++ src/main/java/com/dft/api/shopify/ShopifySdkNew.java
@@ -0,0 +1,87 @@
+package com.dft.api.shopify;
+
+import com.dft.api.shopify.model.auth.AccessCredential;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import static com.dft.api.shopify.constantcode.ConstantCodes.ACCESS_TOKEN_HEADER;
+import static com.dft.api.shopify.constantcode.ConstantCodes.HTTP_HEADER_CONTENT_TYPE;
+import static com.dft.api.shopify.constantcode.ConstantCodes.HTTP_HEADER_VALUE_APPLICATION_JSON;
+import static com.dft.api.shopify.constantcode.ConstantCodes.MAX_ATTEMPTS;
+import static com.dft.api.shopify.constantcode.ConstantCodes.TIME_OUT_DURATION;
+
+public class ShopifySdkNew {
+
+    protected HttpClient client;
+    protected AccessCredential accessCredential;
+    protected ObjectMapper objectMapper;
+
+    public ShopifySdkNew(AccessCredential accessCredential) {
+        client = HttpClient.newHttpClient();
+        objectMapper = new ObjectMapper();
+        this.accessCredential = accessCredential;
+    }
+
+    @SneakyThrows
+    protected HttpRequest get(URI uri) {
+        return HttpRequest.newBuilder(uri)
+            .header(ACCESS_TOKEN_HEADER, this.accessCredential.getAccessToken())
+            .header(HTTP_HEADER_CONTENT_TYPE, HTTP_HEADER_VALUE_APPLICATION_JSON)
+            .GET()
+            .build();
+    }
+
+    @SneakyThrows
+    protected HttpRequest postWithoutAccessToken(URI uri, final String jsonBody) {
+
+        return HttpRequest.newBuilder(uri)
+            .header(HTTP_HEADER_CONTENT_TYPE, HTTP_HEADER_VALUE_APPLICATION_JSON)
+            .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+            .build();
+    }
+
+    @SneakyThrows
+    protected URI addParameters(URI uri, HashMap<String, String> params) {
+
+        String query = uri.getQuery();
+        StringBuilder builder = new StringBuilder();
+        if (query != null)
+            builder.append(query);
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String keyValueParam = entry.getKey() + "=" + entry.getValue();
+            if (!builder.toString().isEmpty())
+                builder.append("&");
+            builder.append(keyValueParam);
+        }
+        return new URI(uri.getScheme(), uri.getAuthority(), uri.getPath(), builder.toString(), uri.getFragment());
+    }
+
+    @SneakyThrows
+    public <T> T getRequestWrapped(HttpRequest request, HttpResponse.BodyHandler<T> handler) {
+
+        return client.sendAsync(request, handler)
+            .thenComposeAsync(response -> tryResend(client, request, handler, response, 1))
+            .get()
+            .body();
+    }
+
+    @SneakyThrows
+    public <T> CompletableFuture<HttpResponse<T>> tryResend(HttpClient client,
+                                                            HttpRequest request,
+                                                            HttpResponse.BodyHandler<T> handler,
+                                                            HttpResponse<T> resp, int count) {
+        if (resp.statusCode() == 409 && count < MAX_ATTEMPTS) {
+            Thread.sleep(TIME_OUT_DURATION);
+            return client.sendAsync(request, handler)
+                .thenComposeAsync(response -> tryResend(client, request, handler, response, count + 1));
+        }
+        return CompletableFuture.completedFuture(resp);
+    }
+}
---
+++ src/main/java/com/dft/api/shopify/constantcode/ConstantCodes.java
@@ -0,0 +1,16 @@
+package com.dft.api.shopify.constantcode;
+
+public class ConstantCodes {
+
+    public static final int TIME_OUT_DURATION = 5000;
+    public static final int MAX_ATTEMPTS = 50;
+
+    public static final String HTTPS = "https://";
+    public static final String ACCESS_TOKEN_HEADER = "X-Shopify-Access-Token";
+    public static final String HTTP_HEADER_CONTENT_TYPE = "content-type";
+    public static final String HTTP_HEADER_VALUE_APPLICATION_JSON = "application/json";
+    public static final String VERSION_2023_01 = "/admin/api/2023-01/";
+
+    public static final String ACCESS_TOKEN_END_POINT = "/admin/oauth/access_token.json";
+    public static final String SHOP_END_POINT = "shop.json";
+}
