d388c60076f6c29a0570f90ff9206361f72b1290
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-07-28 15:51:37 +0530

    sar-4 Create/update shopify order pojo for version 2023-07

--- src/main/java/com/dft/api/shopify/v202307/model/common/ShopifyShopMoney.java
+++ src/main/java/com/dft/api/shopify/v202307/model/common/ShopifyShopMoney.java
@@ -1,6 +1,5 @@
 package com.dft.api.shopify.v202307.model.common;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -8,11 +7,9 @@ import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ShopifyShopMoney {
 
     private Double amount;
-    private String currency;
     private String currencyCode;
 }
