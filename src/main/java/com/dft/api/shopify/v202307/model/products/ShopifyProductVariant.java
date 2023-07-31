72e98465f467ab55f6fa0390989af76f4524b490
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-07-31 19:07:58 +0530

    sar-5 Added barcode field to variant

--- src/main/java/com/dft/api/shopify/v202307/model/products/ShopifyProductVariant.java
+++ src/main/java/com/dft/api/shopify/v202307/model/products/ShopifyProductVariant.java
@@ -21,6 +21,7 @@ public class ShopifyProductVariant {
     private Long productId;
     private String title;
     private Double price;
+    private String barcode;
     private Long inventoryItemId;
     private String inventoryManagement;
     private Integer inventoryQuantity;
