d730bf41b8f3af5dce6f0f9bbb5a795047865767
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-08-02 14:47:17 +0530

    sar-5 Updated ShopifyOrderFulfillment Pojos as per comments

--- src/main/java/com/dft/api/shopify/v202307/model/fullfillment/ShopifyFulfillmentWrapper.java
+++ src/main/java/com/dft/api/shopify/v202307/model/fullfillment/ShopifyFulfillmentWrapper.java
@@ -1,11 +1,9 @@
 package com.dft.api.shopify.v202307.model.fullfillment;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ShopifyFulfillmentWrapper {
 
