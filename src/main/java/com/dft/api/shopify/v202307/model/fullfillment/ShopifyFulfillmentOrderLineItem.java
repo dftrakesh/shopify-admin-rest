e4a819cb22eb0ee8d34e85ca38eab200ae9ddaad
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-08-02 15:28:25 +0530

    sar-5 Removed sl4j annotation and used sneakythrow LocalDateTimeSerializer

--- src/main/java/com/dft/api/shopify/v202307/model/fullfillment/ShopifyFulfillmentOrderLineItem.java
+++ src/main/java/com/dft/api/shopify/v202307/model/fullfillment/ShopifyFulfillmentOrderLineItem.java
@@ -1,10 +1,8 @@
 package com.dft.api.shopify.v202307.model.fullfillment;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class ShopifyFulfillmentOrderLineItem {
 
     private Long id;
