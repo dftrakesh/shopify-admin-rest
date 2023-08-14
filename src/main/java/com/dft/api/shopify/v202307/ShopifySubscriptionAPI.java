b4cf395011d2c6bdfb2e86022d7babecfa6deba6
jinal.panchal <jinal.panchal@dakshyafreondtech.com>
2023-08-14 16:38:08 +0530

    In shopify-admin-rest - add forwardSlash in cancelRecurringApplicationCharge url.

--- src/main/java/com/dft/api/shopify/v202307/ShopifySubscriptionAPI.java
+++ src/main/java/com/dft/api/shopify/v202307/ShopifySubscriptionAPI.java
@@ -8,6 +8,8 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static com.dft.api.shopify.constantcode.ConstantCodes.FORWARD_SLASH;
+
 public class ShopifySubscriptionAPI extends ShopifySdkNew {
 
     private static final String VERSION_2023_07 = "/2023-07";
@@ -25,8 +27,7 @@ public class ShopifySubscriptionAPI extends ShopifySdkNew {
     }
 
     public void cancelRecurringApplicationCharge(Long chargeId) {
-        URI uri = baseUrl(VERSION_2023_07, APPLICATION_CHARGE_ENDPOINT + chargeId);
-
+        URI uri = baseUrl(VERSION_2023_07, APPLICATION_CHARGE_ENDPOINT + FORWARD_SLASH + chargeId);
         HttpRequest request = delete(uri);
         deleteRequestWrapped(request, HttpResponse.BodyHandlers.ofString());
     }
