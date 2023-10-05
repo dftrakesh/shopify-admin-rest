db51f12c2ff87d2f83d15e84c3cab9ae22a70dbe
dhruv.modi <dhruv.modi@dakshyafreondtech.com>
2023-10-05 11:53:55 +0530

    GODAM-44 Initial import for Shopify Changed version in pom.xml file Removed unused import statements and annotations Make getAllProducts method public

--- src/main/java/com/dft/api/shopify/ShopifyProductAPI.java
+++ src/main/java/com/dft/api/shopify/ShopifyProductAPI.java
@@ -1,20 +1,15 @@
 package com.dft.api.shopify;
 
 import com.dft.api.shopify.model.Pagination;
-import com.dft.api.shopify.model.ShopifyPage;
 import com.dft.api.shopify.model.ShopifyProduct;
-import com.dft.api.shopify.model.ShopifyProducts;
 import com.dft.api.shopify.model.auth.AccessCredential;
-import com.dft.api.shopify.model.order.v202307.ShopifyOrder;
 import com.dft.api.shopify.model.product.ShopifyProductWrapper;
 import lombok.SneakyThrows;
-import lombok.extern.log4j.Log4j2;
 
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 import static com.dft.api.shopify.constantcode.ConstantCodes.HTTPS;
@@ -24,7 +19,6 @@ import static com.dft.api.shopify.constantcode.ConstantCodes.DEFAULT_REQUEST_LIM
 import static com.dft.api.shopify.constantcode.ConstantCodes.LIMIT;
 import static com.dft.api.shopify.constantcode.ConstantCodes.PAGE_INFO;
 
-@Log4j2
 public class ShopifyProductAPI extends ShopifySdkNew {
 
     public ShopifyProductAPI(AccessCredential accessCredential) {
@@ -38,7 +32,7 @@ public class ShopifyProductAPI extends ShopifySdkNew {
         return getRequestWrapped(request, ShopifyProductWrapper.class);
     }
 
-    private List<ShopifyProduct> getAllProducts() {
+    public List<ShopifyProduct> getAllProducts() {
         HashMap<String, String> params = new HashMap<>();
         params.put(LIMIT, DEFAULT_REQUEST_LIMIT);
 
