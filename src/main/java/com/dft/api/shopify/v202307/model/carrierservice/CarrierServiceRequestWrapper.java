9d644020ac410bd551a73a44b03284ab2e8db030
Manan Thakkar <manan.thakkar@dakshyafreondtech.com>
2023-10-20 16:18:58 +0530

    Added Carrier service api in new sdk method

---
+++ src/main/java/com/dft/api/shopify/v202307/model/carrierservice/CarrierServiceRequestWrapper.java
@@ -0,0 +1,13 @@
+package com.dft.api.shopify.v202307.model.carrierservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CarrierServiceRequestWrapper {
+    private CarrierServiceRequest carrierService;
+}
