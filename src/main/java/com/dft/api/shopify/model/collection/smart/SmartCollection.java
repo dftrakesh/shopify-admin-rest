563638a3a1050fd21f0af34f79b4e6f5d4d00363
Dhruv.Fefar <dhruv.fefar@dakshyafreondtech.com>
2024-01-08 14:43:42 +0530

    TB-17 change smartCollection respone object

--- src/main/java/com/dft/api/shopify/model/collection/smart/SmartCollection.java
+++ src/main/java/com/dft/api/shopify/model/collection/smart/SmartCollection.java
@@ -3,53 +3,36 @@ package com.dft.api.shopify.model.collection.smart;
 import com.dft.api.shopify.mappers.DateDeserializer;
 import com.dft.api.shopify.model.ShopifyRule;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SmartCollection {
 
-	private String id;
-	private String title;
-	private String handle;
-	private boolean published;
+    private String id;
+    private String title;
+    private String handle;
+    private boolean published;
+    private String bodyHtml;
+    private String publishedScope;
+    private String sortOrder;
+    private String templateSuffix;
 
-	@XmlElement(name = "body_html")
-	private String bodyHtml;
+    @JsonDeserialize(using = DateDeserializer.class)
+    public LocalDateTime publishedAt;
 
-	@XmlElement(name = "published_scope")
-	private String publishedScope;
+    @JsonDeserialize(using = DateDeserializer.class)
+    public LocalDateTime updatedAt;
 
-	@XmlElement(name = "sort_order")
-	private String sortOrder;
+    private Boolean disjunctive;
 
-	@XmlElement(name = "template_suffix")
-	private String templateSuffix;
-
-	@XmlElement(name = "published_at")
-	@JsonDeserialize(using = DateDeserializer.class)
-	public LocalDateTime  publishedAt;
-
-	@XmlElement(name = "updated_at")
-	@JsonDeserialize(using = DateDeserializer.class)
-	public LocalDateTime updatedAt;
-	
-	private Boolean disjunctive;
-	
-	@XmlElement(name = "rules")
     private List<ShopifyRule> rules = new LinkedList<>();
-    
-   }
+}
